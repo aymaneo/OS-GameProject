7e3df193950167f3bb440a64875b0d01f4e2591b
Aymane <aymane@MacBook-Pro-de-Aymane.local>
2025-05-06 22:15:33 +0200

    Implement thread sleep  and update main

--- sextant/main.cpp
+++ sextant/main.cpp
@@ -14,59 +14,43 @@
 #include <sextant/types.h>
 #include <sextant/Synchronisation/Spinlock/Spinlock.h>
 #include <sextant/memoire/segmentation/gdt.h>
-#include <sextant/memoire/Memoire.h>
 #include <sextant/memoire/MemoirePion.h>
 #include <sextant/memoire/pagination/MemoirePhysique.h>
 #include <sextant/memoire/pagination/Pagination.h>
 #include <Applications/Platform.h>
 #include <sextant/vga/vga.h>
 #include <sextant/vga/sprite.h>
+#include <Applications/BalleManager.h>
 
-//#include <Applications/Entity/Entity.h>
-
-
-extern char __e_kernel,__b_kernel, __b_data, __e_data,  __b_stack, __e_load ;
-int i;
-
-extern vaddr_t bootstrap_stack_bottom; //Adresse de début de la pile d'exécution
-extern size_t bootstrap_stack_size;//Taille de la pile d'exécution
+extern char __e_kernel, __b_kernel, __b_data, __e_data, __b_stack, __e_load;
+extern vaddr_t bootstrap_stack_bottom;
+extern size_t bootstrap_stack_size;
 
 Timer timer;
-//paddr_t kernel_core_base,kernel_core_top;
 unsigned long address;
-
-struct cpu_state *ctxt_hello1;
-struct cpu_state *ctxt_hello2;
-struct cpu_state *ctxt_main;
-vaddr_t hello1_stack, hello2_stack;
-char tab1[4096];
-char tab2[4096];
-char tab[30000];
 
 Ecran ecran;
 memoire *InterfaceMemoire;
 Ecran *monEcran = &ecran;
 
-#define PAGINATION_USE 1
-
-
-
-void Sextant_Init(){
+void Sextant_Init()
+{
 	idt_setup();
 	irq_setup();
 	timer.i8254_set_frequency(1000);
 	irq_set_routine(IRQ_TIMER, ticTac);
-	asm volatile("sti\n"); //Autorise les interruptions
+	asm volatile("sti\n");
 	irq_set_routine(IRQ_KEYBOARD, handler_clavier);
-	multiboot_info_t* mbi;
-	mbi = (multiboot_info_t*)address;
+
+	multiboot_info_t *mbi = (multiboot_info_t *)address;
 	gdt_setup();
-	InterfaceMemoire=memoire::nouveau();
-	InterfaceMemoire->mem_setup(& __e_kernel,(mbi->mem_upper<<10) + (1<<20),&ecran);
-	thread_subsystem_setup(bootstrap_stack_bottom,bootstrap_stack_size);
+	InterfaceMemoire = memoire::nouveau();
+	InterfaceMemoire->mem_setup(&__e_kernel, (mbi->mem_upper << 10) + (1 << 20), &ecran);
+	thread_subsystem_setup(bootstrap_stack_bottom, bootstrap_stack_size);
 	sched_subsystem_setup();
 	irq_set_routine(IRQ_TIMER, sched_clk);
-	set_vga_mode13(); 
+
+	set_vga_mode13();
 	set_palette_vga(palette_vga);
 }
 
@@ -74,39 +58,71 @@ void Sextant_Init(){
 #define R_P1 'd'
 #define L_P2 'j'
 #define R_P2 'k'
-void update_plat(void* arg) {
-    Clavier* keyboard = static_cast<Clavier*>(arg);
-    while (true) {
+
+void update_plat(void *arg)
+{
+	Clavier *keyboard = static_cast<Clavier *>(arg);
+	while (true)
+	{
 		char c = keyboard->getchar();
-		if (c == L_P1) {
-            PlatformManager::getInstance().getPlatform1().moveLeft();
-        }else if(c == R_P1) {
-            PlatformManager::getInstance().getPlatform1().moveRight();
-        }else if(c == L_P2) {
-            PlatformManager::getInstance().getPlatform2().moveLeft();
-        }else if (c == R_P2) {
-            PlatformManager::getInstance().getPlatform2().moveRight();
-        }
-        thread_yield();
-    }
+		if (c == L_P1)
+		{
+			PlatformManager::getInstance().getPlatform1().moveLeft();
+		}
+		else if (c == R_P1)
+		{
+			PlatformManager::getInstance().getPlatform1().moveRight();
+		}
+		else if (c == L_P2)
+		{
+			PlatformManager::getInstance().getPlatform2().moveLeft();
+		}
+		else if (c == R_P2)
+		{
+			PlatformManager::getInstance().getPlatform2().moveRight();
+		}
+		thread_yield();
+	}
 }
 
-void update_screen(void* arg) {
-    Ecran* screen = static_cast<Ecran*>(arg);
-    while (true) {
+void update_screen(void *arg)
+{
+	Ecran *screen = static_cast<Ecran *>(arg);
+	while (true)
+	{
 		screen->renderScene();
-        thread_yield();
-    }
+		thread_yield();
+	}
 }
 
-extern "C" void Sextant_main(unsigned long magic, unsigned long addr){
+void update_balle(void *arg)
+{
+	struct time t;
+	t.sec = 0;
+
+	while (true)
+	{
+		BalleManager::getInstance().update();
+		thread_sleep(&t);
+	}
+}
+
+extern "C" void Sextant_main(unsigned long magic, unsigned long addr)
+{
+	address = addr;
+
 	Clavier clavier;
 	Sextant_Init();
-	
-	PlatformManager& manager = PlatformManager::getInstance();
+
+	PlatformManager &manager = PlatformManager::getInstance();
 	ecran.effacerEcran(NOIR);
-	
-	struct thread* event_thread = create_kernel_thread((kernel_thread_start_routine_t) update_plat, (void*) &clavier);
-	struct thread* screen_thread = create_kernel_thread((kernel_thread_start_routine_t) update_screen, (void*) &monEcran);
+
+	Platform &p1 = manager.getPlatform1();
+	BalleManager::getInstance().spawnBall(p1.x, p1.y - 2);
+
+	struct thread *event_thread = create_kernel_thread((kernel_thread_start_routine_t)update_plat, (void *)&clavier);
+	struct thread *screen_thread = create_kernel_thread((kernel_thread_start_routine_t)update_screen, (void *)&monEcran);
+	struct thread *balle_thread = create_kernel_thread((kernel_thread_start_routine_t)update_balle, nullptr);
+
 	thread_yield();
 }
--- sextant/ordonnancements/preemptif/thread.cpp
+++ sextant/ordonnancements/preemptif/thread.cpp
@@ -16,9 +16,8 @@
    USA.
  */
 
-
 #include "thread.h"
-
+#include <sextant/ordonnancements/preemptif/time.h>
 
 /**
  * The identifier of the thread currently running on CPU.
@@ -36,45 +35,51 @@
  */
 static volatile struct thread *current_thread = NULL_THREAD;
 
-extern "C" {
-	void cpu_context_switch(cpu_state**, cpu_state*);
-	void cpu_context_exit_to(struct cpu_state *,cpu_kstate_function_arg1_t *,
-			ui32_t reclaiming_arg) __attribute__((noreturn));
+extern "C"
+{
+	void cpu_context_switch(cpu_state **, cpu_state *);
+	void cpu_context_exit_to(struct cpu_state *, cpu_kstate_function_arg1_t *,
+							 ui32_t reclaiming_arg) __attribute__((noreturn));
 }
 
 static struct thread thread_list[MAX_THREAD];
 stack_t thread_stack[MAX_THREAD];
 
-void sched_clk(int intid) {
-   thread_yield();
+void sched_clk(int intid)
+{
+	thread_yield();
 }
 
-struct thread *thread_get_current(){
+struct thread *thread_get_current()
+{
 	(current_thread->state == THR_RUNNING);
-	return (struct thread*)current_thread;
+	return (struct thread *)current_thread;
 }
 
-inline static sextant_ret_t set_current(struct thread *thr){
+inline static sextant_ret_t set_current(struct thread *thr)
+{
 	current_thread = thr;
 	current_thread->state = THR_RUNNING;
 	return SEXTANT_OK;
 }
 
-sextant_ret_t thread_subsystem_setup(vaddr_t init_thread_stack_base_addr,size_t init_thread_stack_size) {
+sextant_ret_t thread_subsystem_setup(vaddr_t init_thread_stack_base_addr, size_t init_thread_stack_size)
+{
 	struct thread *myself;
 	int i;
 
-	for (i=0;i<MAX_THREAD;i++)
+	for (i = 0; i < MAX_THREAD; i++)
 		thread_list[i].state = EMPTY;
 	/* Allocate a new thread structure for the current running thread */
-	i=0;
-	myself=NULL_THREAD;
-	while (myself==NULL_THREAD && i < MAX_THREAD) {
+	i = 0;
+	myself = NULL_THREAD;
+	while (myself == NULL_THREAD && i < MAX_THREAD)
+	{
 		if (thread_list[i].state == EMPTY)
 			myself = &(thread_list[i]);
 		i++;
 	}
-	if (! myself)
+	if (!myself)
 		return -1;
 	/* Initialize the thread attributes */
 	myself->state = THR_CREATED;
@@ -86,20 +91,23 @@ sextant_ret_t thread_subsystem_setup(vaddr_t init_thread_stack_base_addr,size_t 
 	return SEXTANT_OK;
 }
 
-struct thread * create_kernel_thread(kernel_thread_start_routine_t start_func,void *start_arg){
+struct thread *create_kernel_thread(kernel_thread_start_routine_t start_func, void *start_arg)
+{
 	int i;
 	struct thread *new_thread;
 	void *temp;
 
-	if (! start_func)
+	if (!start_func)
 		return NULL_THREAD;
 	/* Allocate a new thread structure for the current running thread */
-	i=0;
-	new_thread=NULL_THREAD;
-	while(new_thread==NULL_THREAD && i < MAX_THREAD) {
-		if(thread_list[i].state == EMPTY)
+	i = 0;
+	new_thread = NULL_THREAD;
+	while (new_thread == NULL_THREAD && i < MAX_THREAD)
+	{
+		if (thread_list[i].state == EMPTY)
 			new_thread = &(thread_list[i]);
-		else i++;
+		else
+			i++;
 	}
 	if (!new_thread)
 		return NULL_THREAD;
@@ -108,21 +116,24 @@ struct thread * create_kernel_thread(kernel_thread_start_routine_t start_func,vo
 	new_thread->pid = i;
 	/* Allocate the stack for the new thread */
 	temp = thread_stack[i].stack;
-	new_thread->kernel_stack_base_addr = (vaddr_t) temp;
+	new_thread->kernel_stack_base_addr = (vaddr_t)temp;
 	new_thread->kernel_stack_size = THREAD_KERNEL_STACK_SIZE;
-	if (! new_thread->kernel_stack_base_addr) {
+	if (!new_thread->kernel_stack_base_addr)
+	{
 		new_thread->state = EMPTY;
 		return NULL_THREAD;
 	}
 	/* Initialize the CPU context of the new thread */
-	if (SEXTANT_OK != cpu_kstate_init(& new_thread->cpu_state,(cpu_kstate_function_arg1_t*) start_func,
-			(ui32_t) start_arg,new_thread->kernel_stack_base_addr,new_thread->kernel_stack_size,
-			(cpu_kstate_function_arg1_t*) thread_exit,(ui32_t) NULL_THREAD)) {
+	if (SEXTANT_OK != cpu_kstate_init(&new_thread->cpu_state, (cpu_kstate_function_arg1_t *)start_func,
+									  (ui32_t)start_arg, new_thread->kernel_stack_base_addr, new_thread->kernel_stack_size,
+									  (cpu_kstate_function_arg1_t *)thread_exit, (ui32_t)NULL_THREAD))
+	{
 		new_thread->state = EMPTY;
 		return NULL_THREAD;
 	}
 	/* Mark the thread ready */
-	if(SEXTANT_OK != sched_set_ready(new_thread)) {
+	if (SEXTANT_OK != sched_set_ready(new_thread))
+	{
 		new_thread->state = EMPTY;
 		return NULL_THREAD;
 	}
@@ -132,11 +143,13 @@ struct thread * create_kernel_thread(kernel_thread_start_routine_t start_func,vo
 
 /** Function called after thr has terminated. Called from inside the context
  *     of another thread, interrupts disabled */
-static void delete_thread(struct thread *thr){
+static void delete_thread(struct thread *thr)
+{
 	thr->state = EMPTY;
 }
 
-void thread_exit() {
+void thread_exit()
+{
 	struct thread *myself, *next_thread;
 	myself = thread_get_current();
 	/* Prepare to run the next thread */
@@ -144,12 +157,13 @@ void thread_exit() {
 	next_thread = reschedule(myself, false);
 	/* Immediate switch to next thread */
 	set_current(next_thread);
-	cpu_context_exit_to(next_thread->cpu_state,(cpu_kstate_function_arg1_t*) delete_thread,(ui32_t) myself);
+	cpu_context_exit_to(next_thread->cpu_state, (cpu_kstate_function_arg1_t *)delete_thread, (ui32_t)myself);
 }
 
-thread_state_t thread_get_state(struct thread *thr) {
-	if (! thr)
-		thr = (struct thread*)current_thread;
+thread_state_t thread_get_state(struct thread *thr)
+{
+	if (!thr)
+		thr = (struct thread *)current_thread;
 	return thr->state;
 }
 
@@ -157,34 +171,38 @@ thread_state_t thread_get_state(struct thread *thr) {
  * Helper function to initiate a context switch in case the current
  * thread becomes blocked, waiting for a timeout, or calls yield.
  */
-static sextant_ret_t switch_to_next_thread(switch_type_t operation) {
+static sextant_ret_t switch_to_next_thread(switch_type_t operation)
+{
 	struct thread *myself, *next_thread;
 	(current_thread->state == THR_RUNNING);
 	/* Interrupt handlers are NOT allowed to block ! */
-	myself = (struct thread*)current_thread;
+	myself = (struct thread *)current_thread;
 	/* Make sure that if we are to be marked "BLOCKED", we have any reason of effectively being blocked */
-	if(BLOCK_MYSELF == operation)
+	if (BLOCK_MYSELF == operation)
 		myself->state = THR_BLOCKED;
 	/* Identify the next thread */
-	next_thread = reschedule(myself,(YIELD_MYSELF == operation));
+	next_thread = reschedule(myself, (YIELD_MYSELF == operation));
 	/* Avoid context switch if the context does not change */
-	if (myself != next_thread) {
-		//Actual CPU context switch
+	if (myself != next_thread)
+	{
+		// Actual CPU context switch
 		set_current(next_thread);
 		cpu_context_switch(&myself->cpu_state, next_thread->cpu_state);
 	}
-	else/* No context switch but still update ID of current thread */
+	else /* No context switch but still update ID of current thread */
 		set_current(next_thread);
 	return SEXTANT_OK;
 }
 
-sextant_ret_t thread_yield() {
+sextant_ret_t thread_yield()
+{
 	sextant_ret_t retval;
 	retval = switch_to_next_thread(YIELD_MYSELF);
 	return retval;
 }
 
-sextant_ret_t thread_wait() {
+sextant_ret_t thread_wait()
+{
 	sextant_ret_t retval;
 	retval = switch_to_next_thread(WAIT_MYSELF);
 	return retval;
@@ -193,9 +211,19 @@ sextant_ret_t thread_wait() {
 /**
  * Internal sleep timeout management
  */
-struct sleep_timeout_params {
+struct sleep_timeout_params
+{
 	struct thread *thread_to_wakeup;
 	bool timeout_triggered;
 };
 
-
+sextant_ret_t thread_sleep(struct time *delay)
+{
+	// Crude busy-wait-based sleep using repeated yields
+	// In a real OS, you'd register a timeout action and block
+	for (volatile int i = 0; i < 100000; ++i)
+	{
+		thread_yield();
+	}
+	return SEXTANT_OK;
+}
