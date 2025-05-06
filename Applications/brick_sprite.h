935073c92c177d5140b46422c0d8b1f6afce6d41
Genoxis <lebras.gregoire@gmail.com>
2025-05-06 19:41:50 +0000

    brick paginator

--- Applications/brick_sprite.h
+++ Applications/brick_sprite.h
@@ -2,6 +2,7 @@
 #define SPRITE_H
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 #define SPRITE_WIDTH  64
 #define SPRITE_HEIGHT 16
@@ -9,13 +10,16 @@
 <<<<<<<< HEAD:sextant/vga/sprite.h
 #define SPRITE_WIDTH  100
 #define SPRITE_HEIGHT 20
+=======
+>>>>>>> c68d85d (brick paginator)
 
-========
-// Généré par png_to_c_with_palette.py à partir de 'pixel-32x32.png'
 #define SPRITE_WIDTH  64
 #define SPRITE_HEIGHT 16
+<<<<<<< HEAD
 >>>>>>>> bb7a93e (v1):Applications/brick_sprite.h
 >>>>>>> 03ab0c8 (fix rebase)
+=======
+>>>>>>> c68d85d (brick paginator)
 
 extern unsigned char palette_vga[256][3];
 extern unsigned char sprite_data[SPRITE_WIDTH*SPRITE_HEIGHT];
