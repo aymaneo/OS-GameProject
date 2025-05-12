eb67419ebff83a7853f1f9775ff618736b460e3a
Aymane <aymane@MacBook-Pro-de-Aymane.local>
2025-05-12 14:51:01 +0200

    fix brick split

--- Applications/BrickManager.cpp
+++ Applications/BrickManager.cpp
@@ -22,28 +22,22 @@ int BrickManager::getBrickCount() {
 }
 
 void BrickManager::generateBricks(){
-    int space_x = 3;
-	int space_y = 3;
-	int total_x_padding = 320 - (bricksPerLine * BrickManager::brickWidth + (bricksPerLine - 1) * space_x);
-	int padding;
-	if (total_x_padding % 2 == 0){
-		padding = total_x_padding / 2;
-	}else{
-		padding = (1+total_x_padding) / 2;
-	}
+    int space_x = 0;
+    int space_y = 3;
+    int padding = 4;
 
-	int number_of_lines = 4;
+    int number_of_lines = 4;
 
-	int cruseur_x = 0; 
-	int curseur_y = 3;
-	for (int j = 0; j < number_of_lines; j++){
-		cruseur_x = padding;
-		for (int i = 0; i < bricksPerLine; i++){
-			brick_manager_instance->bricks[j * bricksPerLine + i].x = cruseur_x;
-			brick_manager_instance->bricks[j * bricksPerLine + i].y = curseur_y;
-			cruseur_x += BrickManager::brickWidth + space_x;
+    int cruseur_x = 0; 
+    int curseur_y = 3;
+    for (int j = 0; j < number_of_lines; j++){
+        cruseur_x = padding;
+        for (int i = 0; i < bricksPerLine; i++){
+            brick_manager_instance->bricks[j * bricksPerLine + i].x = cruseur_x;
+            brick_manager_instance->bricks[j * bricksPerLine + i].y = curseur_y;
+            cruseur_x += BrickManager::brickWidth + space_x;
             brickCount++;
-		}
-		curseur_y += BrickManager::brickHeight;
-	} 
+        }
+        curseur_y += BrickManager::brickHeight;
+    } 
 }
